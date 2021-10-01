718c361215d0a954a3246e4c26961602d6fefc6f
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-01 11:21:01 +0800

    feat: Add constant class `Paths`

---
+++ src/TemplateMOTDPlugin/Configuration/Paths.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace TemplateMOTDPlugin.Configuration
+{
+    /// <summary>
+    /// Provides constant paths that point to important folders or files.
+    /// </summary>
+    public static class Paths
+    {
+        /// <summary>
+        /// Represents the shared folder where plugins store their configs.
+        /// </summary>
+        public static readonly string GlobalConfigPath = Path.GetFullPath("config");
+
+        /// <summary>
+        /// Represents the folder where this plugin stores its config.
+        /// </summary>
+        public static readonly string SavePath = Path.Combine(GlobalConfigPath, "templatemotd");
+
+        /// <summary>
+        /// Represents the path to the MOTD template.
+        /// </summary>
+        public static readonly string MOTDPath = Path.Combine(SavePath, "motd.txt");
+    }
+}
